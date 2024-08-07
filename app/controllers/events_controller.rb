730ecfd1c6b48a21a03bae3ffc2f6a7a8eba8111
Oscar Tellez <oscartzgz@gmail.com>
2024-08-07 17:35:20 -0600

    WIP: Create events controller and views

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,20 @@
+class EventsController < ApplicationController
+  before_action :validate_organizer
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
